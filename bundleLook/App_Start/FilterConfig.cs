c0ec46549017b66e4fda4e5ef66f61a706337a2c
tofigf <tofigshf@code.edu.az>
2018-07-03 11:28:24 +0400

    multilinq

---
+++ bundleLook/App_Start/FilterConfig.cs
@@ -0,0 +1,15 @@
+﻿using bundleLook.Filters;
+using System.Web;
+using System.Web.Mvc;
+
+namespace bundleLook
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+            filters.Add(new LocalizationAttribute("en"), 0);
+        }
+    }
+}
---
+++ bundleLook/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using bundleLook.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using bundleLook.Models;
+
+namespace bundleLook.Controllers
+{
+    //[Localization("en")]
+    public class HomeController : Controller
+    {
+        MultilingualEntities db = new MultilingualEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            var Lang = Request.RequestContext.RouteData.Values["lang"];
+
+            ViewBag.News = db.News.Where(n=>n.Language==Lang);
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            var BrouserH = Request.Browser.ScreenPixelsHeight;
+            var BrouserW = Request.Browser.ScreenPixelsWidth;
+            var BrouserD = Request.UserLanguages[0];
+
+            ViewBag.Us = BrouserH + " "+ BrouserW+" "+ BrouserD;
+
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
